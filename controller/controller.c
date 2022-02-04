c2fe14f36f0bbdd43dbdb5286c54dd96a92b62f3
z00rk <z00rk@riseup.net>
2022-02-04 19:39:14 +0100

    Update controller.c

--- controller/controller.c
+++ controller/controller.c
@@ -1,14 +1,4 @@
 
-/*
- * 
- *
- * The computer program contained herein contains proprietary
- * information which is the property of Sagemcom Broadband.
- * The program may be used and/or copied only with the written
- * permission of Sagemcom Broadband or in accordance with the
- * terms and conditions stipulated in the agreement/contract under
- * which the programs have been supplied.
- */
 
 #include <pthread.h>
 
