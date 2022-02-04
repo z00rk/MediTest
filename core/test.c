6a0f9ddbae4588ff0271513a305d1e889ba4a3ba
z00rk <z00rk@riseup.net>
2022-02-04 19:38:57 +0100

    Update test.c

--- core/test.c
+++ core/test.c
@@ -1,16 +1,3 @@
-
-/*
- * 
- *
- * The computer program contained herein contains proprietary
- * information which is the property of Sagemcom Broadband.
- * The program may be used and/or copied only with the written
- * permission of Sagemcom Broadband or in accordance with the
- * terms and conditions stipulated in the agreement/contract under
- * which the programs have been supplied.
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
